86d98b58b5ad43918abc74cb2c026eded9e6650d
Balazs Nemeth <bnemeth@redhat.com>
2024-04-26 11:22:36 +0200

    wip

--- daemon/sfc-reconciler/sfc.go
+++ daemon/sfc-reconciler/sfc.go
@@ -3,6 +3,7 @@ package sfcreconciler
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -10,9 +11,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-    corev1 "k8s.io/api/core/v1"
 
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/go-logr/logr"
 	configv1 "github.com/openshift/dpu-operator/api/v1"
@@ -21,13 +21,13 @@ import (
 
 // SfcReconciler reconciles a Service Function Chain object
 type SfcReconciler struct {
-    client.Client
-    Scheme *runtime.Scheme
-    log logr.Logger
+	client.Client
+	Scheme *runtime.Scheme
+	log    logr.Logger
 }
 
 func networkFunctionPod(name string, image string) *corev1.Pod {
-    falseVar := false
+	falseVar := false
 	return &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
@@ -41,6 +41,12 @@ func networkFunctionPod(name string, image string) *corev1.Pod {
 				{
 					Name:  name,
 					Image: image,
+					Ports: []corev1.ContainerPort{
+						{
+							Name:          "web",
+							ContainerPort: 8080,
+						},
+					},
 					Resources: corev1.ResourceRequirements{
 						Requests: corev1.ResourceList{
 							"openshift.io/dpu": resource.MustParse("2"),
@@ -49,13 +55,13 @@ func networkFunctionPod(name string, image string) *corev1.Pod {
 							"openshift.io/dpu": resource.MustParse("2"),
 						},
 					},
-                    SecurityContext: &corev1.SecurityContext{
-                        AllowPrivilegeEscalation: &falseVar,
-                        Capabilities: &corev1.Capabilities{
-                            Drop: []corev1.Capability{"ALL"},
-                            Add:  []corev1.Capability{"NET_RAW"},
-                        },
-                    },
+					SecurityContext: &corev1.SecurityContext{
+						AllowPrivilegeEscalation: &falseVar,
+						Capabilities: &corev1.Capabilities{
+							Drop: []corev1.Capability{"ALL"},
+							Add:  []corev1.Capability{"NET_RAW"},
+						},
+					},
 				},
 			},
 		},
@@ -63,67 +69,65 @@ func networkFunctionPod(name string, image string) *corev1.Pod {
 }
 
 func (r *SfcReconciler) createOrUpdatePod(ctx context.Context, pod *corev1.Pod) error {
-    err := r.Get(ctx, types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, pod)
-    if err != nil && errors.IsNotFound(err) {
-        r.log.Info("Creating Pod", "name", pod.Name)
-        if err := r.Create(ctx, pod); err != nil {
-            r.log.Error(err, "Failed to create Pod", "pod", pod.Name)
-            return err
-        }
-        r.log.Info("Pod created successfully", "pod", pod.Name)
-    } else if err == nil {
-        r.log.Info("Updating Pod", "name", pod.Name)
-        if err := r.Update(ctx, pod); err != nil {
-            r.log.Error(err, "Failed to update Pod", "pod", pod.Name)
-            return err
-        }
-        r.log.Info("Pod updated successfully", "pod", pod.Name)
-    } else {
-        r.log.Error(err, "Failed to get Pod", "pod", pod.Name)
-        return err
-    }
-    return nil
+	err := r.Get(ctx, types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, pod)
+	if err != nil && errors.IsNotFound(err) {
+		r.log.Info("Creating Pod", "name", pod.Name)
+		if err := r.Create(ctx, pod); err != nil {
+			r.log.Error(err, "Failed to create Pod", "pod", pod.Name)
+			return err
+		}
+		r.log.Info("Pod created successfully", "pod", pod.Name)
+	} else if err == nil {
+		r.log.Info("Updating Pod", "name", pod.Name)
+		if err := r.Update(ctx, pod); err != nil {
+			r.log.Error(err, "Failed to update Pod", "pod", pod.Name)
+			return err
+		}
+		r.log.Info("Pod updated successfully", "pod", pod.Name)
+	} else {
+		r.log.Error(err, "Failed to get Pod", "pod", pod.Name)
+		return err
+	}
+	return nil
 }
-
 
 func (r *SfcReconciler) ensureNetworkFunctionExists(ctx context.Context, sfc *configv1.ServiceFunctionChain, nf configv1.NetworkFunction) error {
-    logger := r.log.WithValues("networkFunction", nf.Name)
-    pod := networkFunctionPod(nf.Name, nf.Image)
+	logger := r.log.WithValues("networkFunction", nf.Name)
+	pod := networkFunctionPod(nf.Name, nf.Image)
 
-    if err := controllerutil.SetControllerReference(sfc, pod, r.Scheme); err != nil {
-        logger.Error(err, "Failed to set owner reference on Pod")
-        return err
-    }
+	if err := controllerutil.SetControllerReference(sfc, pod, r.Scheme); err != nil {
+		logger.Error(err, "Failed to set owner reference on Pod")
+		return err
+	}
 
-    if err := r.createOrUpdatePod(ctx, pod); err != nil {
-        logger.Error(err, "Failed to ensure that pod exists")
-        return err
-    }
+	if err := r.createOrUpdatePod(ctx, pod); err != nil {
+		logger.Error(err, "Failed to ensure that pod exists")
+		return err
+	}
 
-    return nil
+	return nil
 }
 
-
 func (r *SfcReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-    r.log = log.FromContext(ctx)
-    r.log.Info("SfcReconciler")
+	r.log = log.FromContext(ctx)
+	r.log.Info("SfcReconciler")
 
-    sfc := &configv1.ServiceFunctionChain{}
-    err := r.Get(ctx, req.NamespacedName, sfc)
-    if err != nil {
-        return ctrl.Result{Requeue: true}, nil
-    }
+	sfc := &configv1.ServiceFunctionChain{}
+	err := r.Get(ctx, req.NamespacedName, sfc)
+	if err != nil {
+		return ctrl.Result{Requeue: true}, nil
+	}
 
-    for _, nf := range sfc.Spec.NetworkFunctions {
-        r.ensureNetworkFunctionExists(ctx, sfc, nf)
-    }
+	for _, nf := range sfc.Spec.NetworkFunctions {
+		r.ensureNetworkFunctionExists(ctx, sfc, nf)
+	}
 
-    return ctrl.Result{}, nil
+	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *SfcReconciler) SetupWithManager(mgr ctrl.Manager) error {
-    return ctrl.NewControllerManagedBy(mgr).
-        For(&configv1.ServiceFunctionChain{}).
-        Complete(r)
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&configv1.ServiceFunctionChain{}).
+		Complete(r)
 }
